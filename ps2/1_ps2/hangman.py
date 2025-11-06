69109beef4988f5f6758d521f362d9fc57a0fbfa
An Dang <andantrun@gmail.com>
2025-11-07 02:02:02 +0700

    Guesses Remaining

--- ps2/1_ps2/hangman.py
+++ ps2/1_ps2/hangman.py
@@ -142,6 +142,7 @@ def hangman(secret_word, with_help):
     guesses_remaining = 10
     letters_guessed = ""
     word_progress = len(secret_word) * "*"
+    vowels = "aeiou"
     print("Welcome to Hangman!")
     print(f"I am thinking of a word that is {len(secret_word)} letters long.")
     while True:
@@ -151,13 +152,19 @@ def hangman(secret_word, with_help):
         print(f"Available letters: {available_letters}")
         guess = input("Please guess a letter: ").lower()
         if guess.isalpha() and len(guess) == 1:
-            letters_guessed += guess
-            word_progress = get_word_progress(secret_word, letters_guessed)
-            if guess in secret_word:
-                print(f"Good guess: {word_progress}")
+            if guess not in letters_guessed:
+                letters_guessed += guess
+                word_progress = get_word_progress(secret_word, letters_guessed)
+                if guess in secret_word:
+                    print(f"Good guess: {word_progress}")
+                else:
+                    if guess not in vowels:
+                        guesses_remaining -= 1
+                    else:
+                        guesses_remaining -= 2
+                    print(f"Oops! That letter is not in my word: {word_progress}")
             else:
-                guesses_remaining -= 1
-                print(f"Oops! That letter is not in my word: {word_progress}")
+                print(f"Oops! You've already guessed that letter: {word_progress}")
         else:
             print(f"Oops! That is not a valid letter. Please input a letter from the alphabet: {word_progress}")
 
